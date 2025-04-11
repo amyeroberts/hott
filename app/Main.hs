1583e736b1cb8aa9415260ccf9f6a4139d7958a7
Amy Roberts <22614925+amyeroberts@users.noreply.github.com>
2025-04-11 17:27:13 -0400

    p1

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,16 @@
 module Main where
 
+
+-- Problem 1
+myLast :: [a] -> Maybe a
+myLast [x] = Just x
+myLast (_:xs) = myLast xs
+myLast [] = Nothing
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    -- Problem
+    let x = myLast [1, 2, 3]
+    print x
+    let y = myLast ([] :: [Int])
+    print y
