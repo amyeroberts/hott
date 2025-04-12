1f528092b748d5380ce3abf130970a76b0e35aef
Amy Roberts <22614925+amyeroberts@users.noreply.github.com>
2025-04-12 17:04:52 -0400

    p2

--- app/Main.hs
+++ app/Main.hs
@@ -7,10 +7,19 @@ myLast [x] = Just x
 myLast (_:xs) = myLast xs
 myLast [] = Nothing
 
+-- Problem 2
+myButLast :: [a] -> Maybe a
+myButLast [x, _] = Just x
+myButLast (_:xs) = myButLast xs
+myButLast [] = Nothing
+
 main :: IO ()
 main = do
-    -- Problem
-    let x = myLast [1, 2, 3]
-    print x
-    let y = myLast ([] :: [Int])
-    print y
+    -- -- Problem
+    -- let x = myLast [1, 2, 3]
+    -- print x
+    -- let y = myLast ([] :: [Int])
+    -- print y
+
+    let z = myButLast ([1, 2, 3, 4, 5] :: [Int])
+    print z
